cd586d01528b646265525287314f1e56bcc77cab
sballew17 <samuel.ballew17@gmail.com>
2021-06-23 21:05:34 -0400

    Create movie-view.jsx

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export class MovieView extends React.Component {
+    render() {
+        const { movieData, onBackClick } = this.props;
+
+        return (
+            <div className="movie-view">
+                <div className="movie-poster">
+                    <img src={movieData.imagePath} />
+                </div>
+                <div className="movie-title">
+                    <span className="label">Title: </span>
+                    <span className="value">{movieData.title}</span>
+                </div>
+                <div className="movie-description">
+                    <span className="label">Description: </span>
+                    <span className="value">{movieData.description}</span>
+                </div>
+                <button onClick={() => { onBackClick(null); }}>Back</button>
+            </div>
+        );
+    }
+}
