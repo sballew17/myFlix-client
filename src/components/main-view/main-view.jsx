8f502f7c26f8dac113a6504ffd24f2471b06c2e5
sballew17 <samuel.ballew17@gmail.com>
2021-06-23 21:05:31 -0400

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
+import { MovieCard } from './myFlix-client/components/movie-card/movie-card';
+import { MovieView } from './myFlix-client/components/movie-view/movie-view';
 
 export class MainView extends React.Component {
     constructor() {
